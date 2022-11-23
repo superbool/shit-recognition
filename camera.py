4f7e8fb2f18e4193d0c579f5414bc551b9b9e906
kefei <kefei@imdada.cn>
2022-11-23 21:57:54 +0800

    获取照片

--- camera.py
+++ camera.py
@@ -1,16 +1,20 @@
 import os
 import time
+from subprocess import call
 
 path = '/data'
-folder = f'{path}/{time.strftime("%Y%m%d")}'
+folder = path + '/' + time.strftime("%Y%m%d")
 
 if not os.path.exists(folder):
     os.mkdir(folder)
-
 # 拍照
-os.system(
-    f'fswebcam -r 1920*1080 --delay 3 --skip 10 {folder}/{time.strftime("%Y%m%d%H%M%S")}.jpg'
-)
+# f'fswebcam -r 1920*1080 --delay 3 --skip 10 {folder}/{time.strftime("%Y%m%d%H%M%S")}.jpg'
 
+def capture():
+    dtime = time.strftime("%Y%m%d%H%M%S")
+    call(["fswebcam", "-d", "/dev/video0", "-r", "1280*720", "--no-banner", "--delay", "3", "--skip", "10",
+          "%s/%s.jpg" % (folder, dtime)])
 
-#* * * * * python /data/camera.py
+capture()
+
+# * * * * * python /data/camera.py
